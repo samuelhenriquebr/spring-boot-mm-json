1240d453bafb54c664bb0e405249bca9dd71e29c
Samuel Gomes <samuelvicosa@gmail.com>
2022-02-01 19:00:29 -0300

    Initial commit

---
+++ src/test/java/guru/springframework/springbootmmjson/SpringBootMmJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springbootmmjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMmJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
